b15b517e7851471c21cabddad160fc56d5382f4e
raiarauju <araujo_raiara@outlook.com>
2024-02-12 20:24:09 -0300

    solving null crash, solving state missing with savedStateHandle and adding swipe component to refresh view

--- app/src/main/java/com/picpay/desafio/android/domain/usecase/FilterListByInputUseCase.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/usecase/FilterListByInputUseCase.kt
@@ -15,9 +15,9 @@ class FilterListByInputUseCase(
             return ResponseState.Success(contactsList)
         }
         val filteredList = contactsList.filter {
-            it.name.split(DELIMITER)
-                .any { fraction -> fraction.lowercase().startsWith(textInput.lowercase()) } ||
-                    it.username.lowercase().startsWith(textInput.lowercase())
+            (it.name?.split(DELIMITER)?.any { fraction ->
+                fraction.lowercase().startsWith(textInput.lowercase()) } ?: false) ||
+                    (it.username?.lowercase()?.startsWith(textInput.lowercase()) ?: false)
         }
         return ResponseState.Success(filteredList)
     }
--- app/src/main/java/com/picpay/desafio/android/presenter/viewmodel/ContactsViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/presenter/viewmodel/ContactsViewModel.kt
@@ -2,31 +2,40 @@ package com.picpay.desafio.android.presenter.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.picpay.desafio.android.data.network.PicPayService
 import com.picpay.desafio.android.data.network.ResponseState
 import com.picpay.desafio.android.domain.usecase.FilterListByInputUseCase
 import com.picpay.desafio.android.domain.usecase.GetContactsUseCase
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class ContactsViewModel(
+    private val savedStateHandle: SavedStateHandle,
     private val getContactsUseCase: GetContactsUseCase,
     private val filterListByInputUseCase: FilterListByInputUseCase
-): ViewModel() {
+) : ViewModel() {
 
-    private val _users : MutableLiveData<ResponseState> = MutableLiveData()
+    private val _users: MutableLiveData<ResponseState> =
+        savedStateHandle.getLiveData("contactsList")
     val users: LiveData<ResponseState> get() = _users
 
     fun getContacts() {
-        _users.value = ResponseState.Loading
-        viewModelScope.launch {
-            val response = getContactsUseCase.getContacts()
-            _users.value = response
+        if (_users.value == null) {
+            refreshContacts()
         }
     }
 
-    fun filterListByInput(textInput : String) {
+    fun refreshContacts() {
+        _users.value = ResponseState.Loading
+        viewModelScope.launch {
+            val response = getContactsUseCase.getContacts()
+            savedStateHandle["contactsList"] = response
+        }
+    }
+
+    fun filterListByInput(textInput: String) {
         val filteredList = filterListByInputUseCase(textInput)
         _users.value = filteredList
     }
