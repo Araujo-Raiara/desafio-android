392fc9ce1de181facbebb1eebc27eb3e79b6f5d6
raiarauju <araujo_raiara@outlook.com>
2024-02-13 20:28:01 -0300

    adding some adjusts

--- app/src/main/java/com/picpay/desafio/android/presenter/adapter/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/presenter/adapter/UserListItemViewHolder.kt
@@ -4,8 +4,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.data.entity.Contact
 import com.picpay.desafio.android.databinding.ListItemUserBinding
-import com.picpay.desafio.android.gone
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 class UserListItemViewHolder(
@@ -16,19 +14,10 @@ class UserListItemViewHolder(
         item.apply {
             name.text = contact.name
             username.text = contact.username
-            progressBar.gone()
         }
         Picasso.get()
             .load(contact.img)
             .error(R.drawable.ic_round_account_circle)
-            .into(item.picture, object : Callback {
-                override fun onSuccess() {
-                    item.progressBar.gone()
-                }
-
-                override fun onError(e: Exception?) {
-                    item.progressBar.gone()
-                }
-            })
+            .into(item.picture)
     }
 }
